ee3f7774c04c5ac0a41e5d8af7659ce59e830ff9
Sareer Ahmed <sareerrajper@gmail.com>
2023-11-25 23:51:45 +0500

    added login

--- Mern2023/Server/src/controllers/auth-controller.js
+++ Mern2023/Server/src/controllers/auth-controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user-model");
 const Category_model = require("../models/category-model");
+const bcrypt = require("bcryptjs"); 
+
 
 const home = async (req, res) => {
     try {
@@ -32,15 +34,48 @@ const register = async (req, res)=> {
         if(userExist){
             return res.status(400).json({msg: "email already regiseterd"}) 
         }else{
+            
           const userCreated = await User.create({fullname, email, phone, password});
-            return res.status(200).json({msg: userCreated});
+            return res.status(200).json({
+                msg: userCreated, 
+                token: await userCreated.generateToken(), 
+                userId: userCreated._id.toString(),
+            });
         }
     } catch (error) {
         res.status(400).json("Enternal error");
     }
 }
 
+const login = async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        const userExist = await User.findOne({email});
+        console.log(userExist);
+        if(!userExist){
+            res.status(400).json({msg: "User is not registred"}); 
+        }
+
+        const user = await bcrypt.compare(password, userExist.password);
+     
+        if(user){
+            res.status(200).json({
+                msg: userExist, 
+                token: await userExist.generateToken(), 
+                userId: userExist._id.toString(),
+                
+            }); 
+        }else{
+            res.status(401).json({message: "Invalid Login Credential"});
+        }
+
+    } catch (error)  {
+        res.status(500).json("Enternal Error");
+    }
+}
 
 
 
-module.exports = {home, register, category};
+
+module.exports = {home, register, category, login};
--- Mern2023/Server/src/models/user-model.js
+++ Mern2023/Server/src/models/user-model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     fullname: {
@@ -22,6 +24,52 @@ const userSchema = new mongoose.Schema({
         default: false,
     },
 });
+
+
+//now bycript using here
+userSchema.pre('save', async function(next){
+     const user = this;
+
+     if(!user.isModified('password')){
+        next();
+     }
+     try {
+        //hash password
+        const saltRound = await bcrypt.genSalt(10);
+        const hash_password = await bcrypt.hash(user.password, saltRound);
+        user.password = hash_password;
+
+     } catch (error) {
+        next(error);
+     }
+
+});
+// password compare
+userSchema.methods.comparePassword = async function(password){
+    return bcrypt.compare(password, this.password);
+};
+
+//  json web token 
+userSchema.methods.generateToken = async function(){
+    try {
+        return jwt.sign({
+            userId: this._id.toString(),
+            email: this.email,
+            role: this.role,
+        },
+            process.env.JWT_SECRET_KEY,
+            {
+                expiresIn:"30d", 
+            }
+
+        );
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
 
 // Pass the model name "User" and the schema to mongoose.model
 const User = mongoose.model("User", userSchema);
